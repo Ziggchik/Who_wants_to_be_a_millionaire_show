75724f400a63f2b8a1f4c6e0775f4158ffc7e0a0
Zigchik <dan.te2012@yandex.ru>
2021-09-08 18:05:52 +0300

    Final version v 1.0

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -2,7 +2,6 @@
 using Who_wants_to_be_a_millionaire_show.Models;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
-//using System.Data.SqlClient;
 
 namespace Who_wants_to_be_a_millionaire_show.Controllers
 {
@@ -10,6 +9,9 @@ namespace Who_wants_to_be_a_millionaire_show.Controllers
     public class QuestionController : Controller
     {
         private QuEntities db = new QuEntities();
+        //Переменные для реализации пагинации
+        //int QuestionNum = 0;
+        //private int QuestionSize = 1;
 
         [Route("index")]
         [Route("")]
@@ -17,7 +19,10 @@ namespace Who_wants_to_be_a_millionaire_show.Controllers
         public IActionResult Index()
         {
             ViewBag.Questions = db.Questions.ToList();
-
+            //Настройка вывода постранчного вывода (структура вопрос-ответ)
+            //ViewData["QuestionNum"] = QuestionNum;
+            //ViewBag.Questions = (from Question in db.Questions orderby Question.IdQuestion select Question).
+            //Skip(QuestionSize * QuestionNum).Take(QuestionSize).ToList();
             return View();
         }
 
@@ -25,7 +30,7 @@ namespace Who_wants_to_be_a_millionaire_show.Controllers
         [Route("submit")]
         public IActionResult Submit(IFormCollection iformCollection)
         {
-            int score = 0;
+            int score = 100;
             string[] questionIDs = iformCollection["QuestionId"];
             foreach (var QuestionId in questionIDs)
             {
@@ -34,7 +39,12 @@ namespace Who_wants_to_be_a_millionaire_show.Controllers
                 if (answeridcorrect == int.Parse(iformCollection["question_"
                     + QuestionId]))
                 {
-                    score++;
+                    score = score * 2;
+                }
+                else
+                {
+                    //условие проигрыша
+                    //score = 0;
                 }
             }
             ViewBag.score = score;
--- Startup.cs
+++ Startup.cs
@@ -13,51 +13,6 @@ namespace Who_wants_to_be_a_millionaire_show
 {
     public class Startup
     {
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        //public IConfiguration Configuration { get; }
-
-        //// This method gets called by the runtime. Use this method to add services to the container.
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //    services.Configure<CookiePolicyOptions>(options =>
-        //    {
-        //        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        //        options.CheckConsentNeeded = context => true;
-        //        options.MinimumSameSitePolicy = SameSiteMode.None;
-        //    });
-
-
-        //    services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        //}
-
-        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //    }
-        //    else
-        //    {
-        //        app.UseExceptionHandler("/Home/Error");
-        //        app.UseHsts();
-        //    }
-
-        //    app.UseHttpsRedirection();
-        //    app.UseStaticFiles();
-        //    app.UseCookiePolicy();
-
-        //    app.UseMvc(routes =>
-        //    {
-        //        routes.MapRoute(
-        //            name: "default",
-        //            template: "{controller=Question}/{action=Index}/{id?}");
-        //    });
-        //}
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
@@ -66,7 +21,6 @@ namespace Who_wants_to_be_a_millionaire_show
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            //services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
